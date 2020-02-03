69cef67c0076b6ab7efc95258e0a16c4f5f13d11
MCB7 <60091696+MCB7@users.noreply.github.com>
2020-02-02 20:40:32 -0800

    Add files via upload

---
+++ Repetition.java
@@ -0,0 +1,34 @@
+//*******************************************************************
+
+//  Class: Repetition
+//  Method: Rep1
+//
+// This class will repeat the string "hello" "n" amount of times depending on the inputed integer "amount"
+// So Rep1("Hello", 2); will output HelloHello
+//By: Morgan Bligh
+//Date: 2/2/2020
+//*******************************************************************
+
+
+package assignment4;
+
+public class Repetition {
+	static void Rep1(String hello, int amount) {
+		
+		for (int i = 0 ; i < amount; i++) {
+		    
+		
+		System.out.print(hello);
+		
+	}
+	
+	}
+	public static void main(String[] args) {
+		
+		   
+		Rep1("hello", 3);
+
+	}
+
+	}
+
